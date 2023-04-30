16c9385adaa4e6ea5218bbac5027909dbf12aa1d
Chirag Kathpalia <chiragkathpalia1@gmail.com>
2023-04-30 20:00:54 +0530

    header_update

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,20 +11,10 @@ function Header() {
 
   return (
     <div className="header">
-      {/* <div id="header__bar" style = {{transform: showHeaderBar? `translateY(-1200px)`: `translateY(0px)`}}>
-        <h3 className="header__bar__title">Frontend Developer</h3>
-        <div className="header__bar__icons">
-          <a href="" target='_blank'><img src="https://img.icons8.com/ios/50/000000/facebook--v1.png"/></a>
-          <a href="" target='_blank'><img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png"/></a>
-          <a href="" target='_blank'><img src="https://img.icons8.com/ios/50/000000/github--v1.png"/></a>
-          <a href="" target='_blank'><img src="https://img.icons8.com/ios/50/000000/linkedin.png"/></a>
-        </div>
-      </div> */}
+      {}
 
       <div className="header-logo"><span>J</span><span className='Sort'>Sort</span></div>
-      {/* <div className="header__options">
-         <h3 className="header__option" onClick = {contactHandler}><span>G</span>et <span>I</span>n <span>T</span>ouch</h3>
-      </div> */}
+      {}
    </div>
   )
 }
